46ad64f2dd14ff1f13f13c85cdfdca2212dbea2a
Juan David <judquinterope@unal.edu.co>
2017-09-07 01:29:42 +0000

    Creacion de la landing page con su html(boostrap) y css(sass)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'welcome/index'
+
   get '/us' => 'about#us'
 
   get '/contact' => 'about#contact'
@@ -13,6 +15,8 @@ Rails.application.routes.draw do
   get '/t_register' => 'user#tec_register'
   
   get '/u_register' => 'user#user_register'
+  
+  root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
