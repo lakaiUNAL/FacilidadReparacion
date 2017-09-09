a36ad3cb6a120a4337c481399b9272e98f13b67b
Alejandro Sierra <gasierram@unal.edu.co>
2017-09-09 00:30:04 +0000

    login corregido

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_name, 
+    :email ])
+    #in keys you list all the input you want to accept.
+  end
 end
