e1eacd13c7760bc29fdee3a732f1d90dcbcfe279
Juan Carlos <jcgamar@unal.edu.co>
2017-09-19 17:53:13 -0500

    Anidar peticiones de los clientes en el namespace clientes

--- app/controllers/cliente/requests_controller.rb
+++ app/controllers/cliente/requests_controller.rb
@@ -1,4 +1,4 @@
-class RequestsController < ApplicationController
+class Cliente::RequestsController < ApplicationController
   before_action :authenticate_customer!
 
   # GET /requests
@@ -10,6 +10,7 @@ class RequestsController < ApplicationController
   # GET /requests/1
   # GET /requests/1.json
   def show
+    @request = Request.find(params[:id])
   end
 
   # GET /requests/new
@@ -19,16 +20,17 @@ class RequestsController < ApplicationController
 
   # GET /requests/1/edit
   def edit
+    @request = Request.find(params[:id])
   end
 
   # POST /requests
   # POST /requests.json
   def create
     @request = Request.new(request_params)
-
+    @request.customer_id = current_customer.id
     respond_to do |format|
-      if @request.save
-        format.html { redirect_to @request, notice: 'Request was successfully created.' }
+      if @request.save #Si fue existosa la creación
+        format.html { redirect_to cliente_requests_url, notice: 'Request was successfully created.' }
         format.json { render :show, status: :created, location: @request }
       else
         format.html { render :new }
@@ -54,9 +56,9 @@ class RequestsController < ApplicationController
   # DELETE /requests/1
   # DELETE /requests/1.json
   def destroy
-    @request.destroy
+    set_request.destroy
     respond_to do |format|
-      format.html { redirect_to requests_url, notice: 'Request was successfully destroyed.' }
+      format.html { redirect_to cliente_requests_url, notice: 'Request was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -69,6 +71,6 @@ class RequestsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def request_params
-      params.require(:request).permit(:customer, :article, :description, :tipe, :address)
+      params.require(:request).permit( :article, :service_tipe, :description, :address)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-
-  resources :requests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
-  
+
   #Paginas estaticas
   get '/blog' => 'static_pages#blog', as: 'blog'
   get '/nosotros' => 'static_pages#about', as: 'about'
   get '/contacto' => 'static_pages#contact', as: 'contact'
   get '/registro' => 'static_pages#register', as: 'register'
   get '/iniciar_sesion' => 'static_pages#start_session', as: 'start_session'
+  
+  namespace :cliente do
+    resources :requests
+  end
 
   devise_for :workers, controllers:{registrations: 'workers/registrations', sessions: 'workers/sessions'} 
   devise_for :customers, controllers:{registrations: 'customers/registrations', sessions: 'customers/sessions'}
