9a7a348201faf980e3748e610a41c764ea3fca35
Felipe <dafrodriguezro@unal.edu.co>
2017-09-20 11:47:09 -0500

    modificada validacion en request, todas las validaciones en primera instancia estan comentariadas

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,6 +2,6 @@ class Request < ApplicationRecord
 	belongs_to :customer
 	belongs_to :service
 	#validates :service_id, presence: true
-	#validates :article, presence: true
+	#validates :legacy_code, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
 	#validates :description, presence: true	
 end
