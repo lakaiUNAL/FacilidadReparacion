fe8c13a9dec820d6809101d6fbf9174f1e44bf21
Diego Cruz <diacruzmo@unal.edu.co>
2017-09-23 19:42:44 +0000

    Editar clientes con Estilos, Alejo y Diego

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_name, 
-    :email ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :user_name, :email ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :user_name, :email, :birth_date, 
+                                                           :number_card, :address, :phone_number])
+
     #in keys you list all the input you want to accept.
   end
   
