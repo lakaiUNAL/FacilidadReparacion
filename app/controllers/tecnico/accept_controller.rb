8763925e7fbecdb8f463aab25780b760a69dec51
David Felipe Rodriguez Rodriguez <felipedavid109@gmail.com>
2017-10-23 13:44:21 +0000

    Problema de poder aceptar dos veces el mismo servicio, arreglado

--- app/controllers/tecnico/accept_controller.rb
+++ app/controllers/tecnico/accept_controller.rb
@@ -10,6 +10,10 @@ class Tecnico::AcceptController < ApplicationController
     worker = current_worker.id
     req = params[:request]
     fecha = params[:date]
-    @shd = Schedule.create(:worker_id => worker, :customer_id => client_id, :request_id => req, :date => fecha)
+    if (Schedule.exists?(worker_id: worker,customer_id: client_id,request_id: req)) == false
+      @shd = Schedule.create(:worker_id => worker, :customer_id => client_id, :request_id => req, :date => fecha)
+    else
+      @shd = 0
+    end
   end
 end
